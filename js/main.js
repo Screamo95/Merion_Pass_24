f49a8432ab6bc06405929f2ea3f829234f38fad1
Screamo95 <bykovglobal@gmail.com>
2024-09-13 14:24:52 +0300

    add program

--- js/main.js
+++ js/main.js
@@ -19,3 +19,25 @@ linkBurger.onclick = function() {
   burgerMenu.classList.remove(`burger__menu-open`)
   burgerMenu.classList.add(`burger__menu-close`)
 }
+
+// Разввернуть, свернуть для блока program
+
+let openProgram = document.querySelector(`.program__open`)
+let closeProgram = document.querySelector(`.program__close`)
+let blockProgram = document.querySelector(`.program__body_close`)
+
+openProgram.onclick = function() {
+  blockProgram.classList.remove(`program__visible`)
+  blockProgram.classList.add(`program__start_animation`)
+  blockProgram.classList.remove(`program__end_animation`)
+  openProgram.classList.add(`program__visible`)
+  closeProgram.classList.remove(`program__visible`)
+}
+
+closeProgram.onclick = function() {
+  blockProgram.classList.remove(`program__start_animation`)
+  blockProgram.classList.add(`program__end_animation`)
+  openProgram.classList.remove(`program__visible`)
+  closeProgram.classList.add(`program__visible`)
+  setTimeout("blockProgram.classList.add(`program__visible`)", 1000)
+}
