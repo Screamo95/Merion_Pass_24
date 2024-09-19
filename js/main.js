f416b8a16eef938c5a67e186314f271fa6e19129
Screamo95 <bykovglobal@gmail.com>
2024-09-19 17:25:03 +0300

    add program full screen

--- js/main.js
+++ js/main.js
@@ -25,11 +25,19 @@ linkBurger.onclick = function() {
 let openProgram = document.querySelector(`.program__open`)
 let closeProgram = document.querySelector(`.program__close`)
 let blockProgram = document.querySelector(`.program__body_close`)
+let blockProgramTable = document.querySelector(`.program__body_close-table`)
+let blockProgramMobile = document.querySelector(`.program__body_close-mobile`)
 
 openProgram.onclick = function() {
   blockProgram.classList.remove(`program__visible`)
   blockProgram.classList.add(`program__start_animation`)
   blockProgram.classList.remove(`program__end_animation`)
+  blockProgramTable.classList.remove(`program__visible`)
+  blockProgramTable.classList.add(`program__start_animation`)
+  blockProgramTable.classList.remove(`program__end_animation`)
+  blockProgramMobile.classList.remove(`program__visible`)
+  blockProgramMobile.classList.add(`program__start_animation`)
+  blockProgramMobile.classList.remove(`program__end_animation`)
   openProgram.classList.add(`program__visible`)
   closeProgram.classList.remove(`program__visible`)
 }
@@ -37,7 +45,13 @@ openProgram.onclick = function() {
 closeProgram.onclick = function() {
   blockProgram.classList.remove(`program__start_animation`)
   blockProgram.classList.add(`program__end_animation`)
-  openProgram.classList.remove(`program__visible`)
+  blockProgramTable.classList.remove(`program__start_animation`)
+  blockProgramTable.classList.add(`program__end_animation`)
+  blockProgramMobile.classList.remove(`program__start_animation`)
+  blockProgramMobile.classList.add(`program__end_animation`)
   closeProgram.classList.add(`program__visible`)
+  openProgram.classList.remove(`program__visible`)
   setTimeout("blockProgram.classList.add(`program__visible`)", 1000)
+  setTimeout("blockProgramTable.classList.add(`program__visible`)", 1000)
+  setTimeout("blockProgramMobile.classList.add(`program__visible`)", 1000)
 }
